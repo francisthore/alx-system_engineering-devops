a10d536f273337185c820463c25c9bbba9d84be0
Francis Thore <thorefrancis@gmail.com>
2024-02-26 21:07:05 +0200

    flask?

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
 # this manifest installs flask
 package { 'flask':
+  require  => Package['python3-pip'],
   ensure   => '2.1.0',
   provider => 'pip3',
+}
+
+package { ['python3-pip']:
+  ensure  => 'installed',
 }
