cd65dbfe72cde01a0ddc0a3f4ae33a0b106b8388
Francis <125464911+francisthore@users.noreply.github.com>
2024-02-28 23:33:49 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,9 @@
 # this manifest installs flask
+package { 'werkzeug' :
+  ensure   => '2.1.1',
+  provider => 'pip3'
+}
 package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
-  name     => 'flask',
+  provider => 'pip3'
 }
