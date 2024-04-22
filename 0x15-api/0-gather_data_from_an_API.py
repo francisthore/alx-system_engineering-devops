52865e1b2d9fd5bd969a947e7eaba6fd48183a09
Francis Thore <thorefrancis@gmail.com>
2024-04-23 00:22:44 +0200

    l

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,22 +7,15 @@ if __name__ == "__main__":
     user_url = 'https://jsonplaceholder.typicode.com/users/{}/'.format(argv[1])
     todo_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
         argv[1])
-    user_request = requests.get(user_url)
-    todo_request = requests.get(todo_url)
+    user_response = requests.get(user_url).json()
+    todo_response = requests.get(todo_url).json()
     try:
-        user_response = user_request.json()
-        todo_response = todo_request.json()
-        i = 0
-        for task in todo_response:
-            if task['completed'] is True:
-                i = i + 1
         name = user_response['name']
+        completed = [task for task in todo_response if task['completed']]
         print('Employee {} is done with tasks({}/{}):'.format(
-            name,
-            i, len(todo_response)
+            name, len(completed), len(todo_response)
         ))
-        for task in todo_response:
-            if task['completed'] is True:
-                print('\t{}'.format(task['title']))
+        for task in completed:
+            print('\t{}'.format(task['title']))
     except Exception as e:
         print(e)
