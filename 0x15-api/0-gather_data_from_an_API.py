ca6513ad4e1f8617a16222fe28cf430d35dd8043
Francis Thore <thorefrancis@gmail.com>
2024-06-03 21:03:26 +0200

    if name main

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,23 +8,24 @@ Usage: ./module <id>
 import requests
 from sys import argv
 
-emp_id = argv[1]
-if type(int(emp_id)) is int:
+if __name__ == '__main__':
+
+    emp_id = argv[1]
     task_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
         emp_id)
     emp_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(emp_id)
 
-with requests.get(task_url) as request:
-    todos = request.json()
-with requests.get(emp_url) as request:
-    emp = request.json()
+    with requests.get(task_url) as request:
+        todos = request.json()
+    with requests.get(emp_url) as request:
+        emp = request.json()
 
-if todos and emp:
-    total_tasks = len(todos)
-    tasks_completed = [task for task in todos if task['completed'] is True]
-    emp_name = emp.get('name')
-    print('Employee {} is done with tasks({}/{}):'.format(
-        emp_name, len(tasks_completed), total_tasks
-    ))
-    for task in tasks_completed:
-        print('\t {}'.format(task.get('title')))
+    if todos and emp:
+        total_tasks = len(todos)
+        tasks_completed = [task for task in todos if task['completed'] is True]
+        emp_name = emp.get('name')
+        print('Employee {} is done with tasks({}/{}):'.format(
+            emp_name, len(tasks_completed), total_tasks
+        ))
+        for task in tasks_completed:
+            print('\t {}'.format(task.get('title')))
