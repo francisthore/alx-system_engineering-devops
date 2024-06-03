36fdc634eb74d3bbeade084a6126b5338826e50d
Francis Thore <thorefrancis@gmail.com>
2024-06-03 20:56:49 +0200

    first try api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+    Retrieves a TODO list progress for a
+    given employee ID
+
+    Usage: ./module <id>
+"""
+
+import requests
+from sys import argv
+
+emp_id = argv[1]
+if type(int(emp_id)) is int:
+    task_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
+        emp_id)
+    emp_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(emp_id)
+
+with requests.get(task_url) as request:
+    todos = request.json()
+with requests.get(emp_url) as request:
+    emp = request.json()
+
+if todos and emp:
+    total_tasks = len(todos)
+    tasks_completed = [task for task in todos if task['completed'] is True]
+    emp_name = emp.get('name')
+    print('Employee {} is done with tasks({}/{}):'.format(
+        emp_name, len(tasks_completed), total_tasks
+    ))
+    for task in tasks_completed:
+        print('\t {}'.format(task.get('title')))
