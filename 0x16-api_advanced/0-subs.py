1c7b92be651b261ad05ebd86e0267f4517d43458
Francis Thore <thorefrancis@gmail.com>
2024-06-18 23:11:41 +0200

    try/except

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,26 @@
 #!/usr/bin/python3
 """
-Makes a GET request to the redit api and returns the number
-of subscribers for a given subredit
+Makes a GET request to the Reddit API and returns the number
+of subscribers for a given subreddit.
 """
 import requests
 
 
-def number_of_subscribers(subreddiit):
-    """Function to make a GET request to the reditapi"""
-    if subreddiit is None:
+def number_of_subscribers(subreddit):
+    """Function to make a GET request to the Reddit API"""
+    if subreddit is None:
         return 0
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddiit)
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {
         'User-Agent': 'mamba'
     }
 
-    with requests.get(url, headers=headers, allow_redirects=False) as res:
-        if res.status_code == 200:
-            res = res.json()
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json().get('data', {})
+            return data.get('subscribers', 0)
         else:
             return 0
-
-    if 'subscribers' not in res.get('data'):
+    except requests.RequestException:
         return 0
-    else:
-        return res.get('data').get('subscribers')
