ec743ab52575252860833c7c385def955229a63f
Francis Thore <thorefrancis@gmail.com>
2024-06-18 23:29:50 +0200

    top 10

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Module that queries the Reddit API and prints the
+titles of the first 10 hot posts listed for a given subreddit.
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """Function to make a GET request to the Reddit API"""
+    if subreddit is None:
+        print(None)
+        return
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {
+        'User-Agent': 'mamba'
+    }
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            data = data['data']['children']
+            for post in data:
+                print(post.get('data').get('title', None))
+        else:
+            print(None)
+            return
+    except requests.RequestException:
+        print(None)
+        return
