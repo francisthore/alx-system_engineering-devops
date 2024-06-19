85607ce7688e194898088a540ca241fb4461c503
Francis Thore <thorefrancis@gmail.com>
2024-06-19 22:19:49 +0200

    webstack debug3

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,51 @@
+# Fixes wordpress config
+class webstack_debugging_3 {
+  # Create /var/www/html/.maintenance file
+  file { '/var/www/html/.maintenance':
+    ensure => present,
+    mode   => '0644',
+  }
+
+  # Ensure wp-content/languages directory exists
+  file { '/var/www/html/wp-content/languages':
+    ensure => directory,
+    mode   => '0755',
+  }
+
+  # Ensure wp-includes/languages directory exists
+  file { '/var/www/html/wp-includes/languages':
+    ensure => directory,
+    mode   => '0755',
+  }
+
+  # Create /var/www/html/wp-content/db.php file
+  file { '/var/www/html/wp-content/db.php':
+    ensure => present,
+    mode   => '0644',
+  }
+
+  # Create /var/www/html/wp-content/object-cache.php file
+  file { '/var/www/html/wp-content/object-cache.php':
+    ensure => present,
+    mode   => '0644',
+  }
+
+  # Fix the typo in wp-settings.php
+  augeas { 'fix_wp_locale_typo':
+    context => '/files/var/www/html/wp-settings.php',
+    changes => [
+      "set *[contains(text(), 'class-wp-locale.phpp')]/text 'require_once(ABSPATH . WPINC . \"/class-wp-locale.php\");'"
+    ],
+    onlyif  => "match *[contains(text(), 'class-wp-locale.phpp')] size > 0",
+  }
+
+  # Ensure the Apache service is running and restart it if necessary
+  service { 'apache2':
+    ensure    => 'running',
+    enable    => true,
+    subscribe => Augeas['fix_wp_locale_typo'],
+  }
+}
+
+# Include the class to apply the fix
+include webstack_debugging_3
