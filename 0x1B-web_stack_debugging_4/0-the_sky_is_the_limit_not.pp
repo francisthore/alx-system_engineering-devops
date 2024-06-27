d8f09e51d55b12119ce13417d4d9529ee65e1abb
Francis Thore <thorefrancis@gmail.com>
2024-06-27 20:07:09 +0000

    puppet lint

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,13 @@
 # Puppet manifest to fix nging ULIMIT
 exec { 'replace_15_with_4096':
-	command => 'sed -i "s/15/4096/g" /etc/default/nginx',
-	path    => ['/bin', '/usr/bin'],
-	notify  => Exec['restart_nginx'],
+  command => 'sed -i "s/15/4096/g" /etc/default/nginx',
+  path    => ['/bin', '/usr/bin'],
+  notify  => Exec['restart_nginx'],
 }
 
 # Restart nginx
 exec {'restart_nginx':
-	command     => 'service nginx restart',
-	path        => ['/bin', '/usr/bin'],
-	refreshonly => true,
+  command     => 'service nginx restart',
+  path        => ['/bin', '/usr/bin'],
+  refreshonly => true,
 }
