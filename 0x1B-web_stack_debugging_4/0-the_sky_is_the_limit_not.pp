31622ea1dc3d74966421e7b63baa1a7b7de9a27d
Francis Thore <thorefrancis@gmail.com>
2024-06-27 20:02:15 +0000

    path

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,13 @@
 # Puppet manifest to fix nging ULIMIT
 exec { 'replace_15_with_4096':
 	command => 'sed -i "s/15/4096/g" /etc/default/nginx',
-	onlyif  => 'grep -q "15" /etc/default/nginx',
+	path    => ['/bin', '/usr/bin'],
 	notify  => Exec['restart_nginx'],
 }
 
 # Restart nginx
 exec {'restart_nginx':
 	command     => 'service nginx restart',
+	path        => ['/bin', '/usr/bin'],
 	refreshonly => true,
 }
